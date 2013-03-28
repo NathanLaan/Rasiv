3b6352b74ae2403ebe3df42eb0ee070f7b8667f5
nathanlaan <nathanlaan@gmail.com>
2013-03-28 00:24:35 -0600

    Experimenting with EMBER and HANDLEBARS.

--- Rasiv.Web/js/app.js
+++ Rasiv.Web/js/app.js
@@ -1,12 +1,39 @@
-﻿var RasivApp = Ember.Application.create();
+﻿var App = Ember.Application.create();
 
+Ember.LOG_BINDINGS = true;
 
-RasivApp.IndexController = Ember.Controller.extend({
-    appHeader: "HeaderText",
-    appBody: "BodyText"
+App.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+});
+App.ApplicationController = Ember.Controller.extend();
+
+App.AllContributorsController = Ember.ArrayController.extend();
+App.AllContributorsView = Ember.View.extend({
+    templateName: 'contributors'
+});
+
+App.Router = Ember.Router.extend({
+    enableLogging: true,
+    root: Ember.Route.extend({
+        index: Ember.Route.extend({
+            route: '/',
+            connectOutlets: function (router) {
+                console.log('INDEX');
+                router.get('applicationController').connectOutlet('allContributors', [{ login: 'wycats' }, { login: 'tomdale'}]);
+            }
+        })
+    })
 });
 
 
-RasivApp.Router.map(function () {
-    this.route('about');
-});
+
+
+//RasivApp.IndexController = Ember.Controller.extend({
+//    appHeader: "HeaderText",
+//    appBody: "BodyText"
+//});
+
+
+//RasivApp.Router.map(function () {
+//    this.route('about');
+//});
