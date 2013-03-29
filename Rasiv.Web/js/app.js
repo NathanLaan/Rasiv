b2b35ed6efc7dfe2e12d525784669e7777ddbf2d
nathanlaan <nathanlaan@gmail.com>
2013-03-28 21:25:07 -0600

    Switching from EMBER to BACKBONE.

--- Rasiv.Web/js/app.js
+++ Rasiv.Web/js/app.js
@@ -1,39 +1,68 @@
-﻿var App = Ember.Application.create();
+﻿$(function () {
 
-Ember.LOG_BINDINGS = true;
 
-App.ApplicationView = Ember.View.extend({
-    templateName: 'application'
+
+    RasivFeedModel = Backbone.Model.extend({
+        initialize: function () {
+            console.log('RasivAppModel.initialize()');
+        },
+        addFeed: function (f) {
+            console.log("RasivAppModel.addFeed(): " + f);
+        }
+    });
+
+    AddFeedView = Backbone.View.extend({
+        initialize: function () {
+            console.log('addFeedView.initialize()');
+            this.renderView();
+        },
+        renderView: function () {
+            //Pass variables in using Underscore.js Template
+            var variables = { addFeedLabel: "Add Feed" };
+            // Compile the template using underscore
+            var template = _.template($("#addFeedTemplate").html(), variables);
+            // Load the compiled HTML into the Backbone "el"
+            this.$el.html(template);
+        },
+        events: {
+            "click input[type=button]": "addFeed_EventHandler"
+        },
+        addFeed_EventHandler: function (event) {
+            // event.currentTarget
+            rfm.addFeed($("#addFeed_URL").val());
+        }
+    });
+
+    var rfm = new RasivFeedModel();
+
+
+
+
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "add": "addRoute",
+            "*actions": "defaultRoute" // matches http://rasiv.com/#anything-here
+        }
+    });
+    // Initiate the router
+    var RasivAppRouter = new AppRouter;
+
+    RasivAppRouter.on('route:defaultRoute', function (actions) {
+        console.log('ACTIONS: ' + actions);
+    });
+
+    RasivAppRouter.on('route:addRoute', function (id) {
+        console.log('route:addRoute--1');
+        var afv = new AddFeedView({ el: $("#pageContent") });
+        afv.initialize();
+        console.log('route:addRoute--2');
+    });
+
+    // Start Backbone history a necessary step for bookmarkable URL's
+    Backbone.history.start();
+
+
+
+
 });
-App.ApplicationController = Ember.Controller.extend();
-
-App.AllContributorsController = Ember.ArrayController.extend();
-App.AllContributorsView = Ember.View.extend({
-    templateName: 'contributors'
-});
-
-App.Router = Ember.Router.extend({
-    enableLogging: true,
-    root: Ember.Route.extend({
-        index: Ember.Route.extend({
-            route: '/',
-            connectOutlets: function (router) {
-                console.log('INDEX');
-                router.get('applicationController').connectOutlet('allContributors', [{ login: 'wycats' }, { login: 'tomdale'}]);
-            }
-        })
-    })
-});
-
-
-
-
-//RasivApp.IndexController = Ember.Controller.extend({
-//    appHeader: "HeaderText",
-//    appBody: "BodyText"
-//});
-
-
-//RasivApp.Router.map(function () {
-//    this.route('about');
-//});
