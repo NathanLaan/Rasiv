c3613f65be5b676af97e077887e543fcd952fd5a
nathanlaan <nathanlaan@gmail.com>
2013-03-30 13:23:35 -0600

    Converting project to Visual Studio 2012.

---
+++ Rasiv.Web/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Rasiv.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            /*
+            Response.Write("URL: " + Request.Url);
+            Response.Write("<br/>URL: " + Request.Url.AbsolutePath);
+            if (Request.Url.Equals("") || 
+                Request.Url.AbsolutePath.Equals("/default.aspx") || 
+                Request.Url.AbsolutePath.Equals("/"))
+            {
+                Server.Transfer("/index.html");
+            }
+            */
+        }
+
+    }
+}
